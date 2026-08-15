8caf406befdc1c7ed10a9762abf84ab2a6961f1b
KrushnaSPPUDoT-code <krushnasppu@gmail.com>
2026-08-15 06:38:58 +0000

    feat: implement private student eligibility credential

--- contract/src/test/bboard-simulator.ts
+++ contract/src/test/bboard-simulator.ts
@@ -1,49 +1,48 @@
-// This file is part of midnightntwrk/example-bboard.
-// Copyright (C) Midnight Foundation
-// SPDX-License-Identifier: Apache-2.0
-// Licensed under the Apache License, Version 2.0 (the "License");
-// You may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 import {
   type CircuitContext,
   QueryContext,
   sampleContractAddress,
-  convertFieldToBytes,
   createConstructorContext,
   CostModel,
 } from "@midnight-ntwrk/compact-runtime";
+
 import {
   Contract,
   type Ledger,
   ledger,
 } from "../managed/bboard/contract/index.js";
-import { type BBoardPrivateState, witnesses } from "../witnesses.js";
 
-/**
- * Serves as a testbed to exercise the contract in tests
- */
+import {
+  type BBoardPrivateState,
+  witnesses,
+} from "../witnesses.js";
+
 export class BBoardSimulator {
   readonly contract: Contract<BBoardPrivateState>;
   circuitContext: CircuitContext<BBoardPrivateState>;
 
-  constructor(secretKey: Uint8Array) {
+  constructor(
+    secretKey: Uint8Array,
+    cgpa = 850n,
+    attendance = 85n,
+  ) {
     this.contract = new Contract<BBoardPrivateState>(witnesses);
+
     const {
       currentPrivateState,
       currentContractState,
       currentZswapLocalState,
     } = this.contract.initialState(
-      createConstructorContext({ secretKey }, "0".repeat(64)),
+      createConstructorContext(
+        {
+          secretKey,
+          cgpa,
+          attendance,
+        },
+        "0".repeat(64),
+      ),
     );
+
     this.circuitContext = {
       currentPrivateState,
       currentZswapLocalState,
@@ -55,17 +54,6 @@ export class BBoardSimulator {
     };
   }
 
-  /***
-   * Switch to a different secret key for a different user
-   *
-   * TODO: is there a nicer abstraction for testing multi-user dApps?
-   */
-  public switchUser(secretKey: Uint8Array) {
-    this.circuitContext.currentPrivateState = {
-      secretKey,
-    };
-  }
-
   public getLedger(): Ledger {
     return ledger(this.circuitContext.currentQueryContext.state);
   }
@@ -74,32 +62,27 @@ export class BBoardSimulator {
     return this.circuitContext.currentPrivateState;
   }
 
-  public post(message: string): Ledger {
-    // Update the current context to be the result of executing the circuit.
-    this.circuitContext = this.contract.impureCircuits.post(
-      this.circuitContext,
-      message,
-    ).context;
-    return ledger(this.circuitContext.currentQueryContext.state);
+  public issueCredential(): Ledger {
+    this.circuitContext =
+      this.contract.impureCircuits.issueCredential(
+        this.circuitContext,
+      ).context;
+
+    return this.getLedger();
   }
 
-  public takeDown(): Ledger {
-    this.circuitContext = this.contract.impureCircuits.takeDown(
+  public proveEligibility(): boolean {
+    return this.contract.impureCircuits.proveEligibility(
       this.circuitContext,
-    ).context;
-    return ledger(this.circuitContext.currentQueryContext.state);
-  }
-
-  public publicKey(): Uint8Array {
-    const sequence = convertFieldToBytes(
-      32,
-      this.getLedger().sequence,
-      "bboard-simulator.ts",
-    );
-    return this.contract.circuits.publicKey(
-      this.circuitContext,
-      this.getPrivateState().secretKey,
-      sequence,
     ).result;
+  }
+
+  public revokeCredential(): Ledger {
+    this.circuitContext =
+      this.contract.impureCircuits.revokeCredential(
+        this.circuitContext,
+      ).context;
+
+    return this.getLedger();
   }
 }
